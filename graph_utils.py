e46fc8e5c564b489b07fb127959dc5b62b580ee7
hongxuenong <hongxuenong@gmail.com>
2020-05-23 17:03:08 +0800

    First commit

---
+++ graph_utils.py
@@ -0,0 +1,328 @@
+from pyparsing import *
+import dgl
+import networkx as nx
+import matplotlib.pyplot as plt
+from math import sqrt
+
+import torch
+from tqdm import tqdm
+from torch_geometric.nn import MessagePassing
+from torch_geometric.data import Data
+from torch_geometric.utils import k_hop_subgraph, to_networkx
+import os
+import numpy as np
+
+def build_circuit_graph_undirected(node_list,edge_list):
+    g = dgl.DGLGraph()
+    g.add_nodes(len(node_list))
+    src, dst = tuple(zip(*edge_list))
+    g.add_edges(src, dst)
+    g.add_edges(dst, src)
+    
+    return g
+
+def build_circuit_graph_directed_sd(node_list,edge_list):
+    g = dgl.DGLGraph()
+    g.add_nodes(len(node_list))
+    src, dst = tuple(zip(*edge_list))
+    g.add_edges(src, dst)
+    return g
+
+def build_circuit_graph_directed_ds(node_list,edge_list):
+    g = dgl.DGLGraph()
+    g.add_nodes(len(node_list))
+    src, dst = tuple(zip(*edge_list))
+    g.add_edges(dst, src)
+    return g
+    
+
+def VisualizeGraph(graph=None,node_labels=None,mode='dgl',ax=None):
+    if mode =='dgl':
+        graph = graph.to_networkx()
+
+    elif mode =='adj':
+        graph = nx.Graph(graph)
+
+    if graph is not None:
+
+        # Kamada-Kawaii layout usually looks pretty for arbitrary graphs
+        pos = nx.kamada_kawai_layout(graph)
+        plt.figure(1,figsize=(12,12))
+        cmap=None
+        if node_labels is not None:
+            cmap = plt.get_cmap('gist_rainbow',len(np.unique(node_labels)))
+            
+            cmap.set_under('gray')
+        vmin = 0
+        vmax = len(np.unique(node_labels)) if node_labels is not None else 37
+        nx.draw(graph,pos,with_labels=False,node_size=500,font_size=14,node_color=node_labels,cmap=cmap,vmin = vmin,vmax=vmax,ax=ax)
+        # plt.savefig('c432_undirected.png')
+#         nx.draw_networkx_labels(nx_G_directed_sd,pos,labels=gate_type_dict2)
+        nx.draw_networkx_labels(graph,pos,ax=ax)
+        if cmap is not None:
+            cm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin = vmin, vmax=vmax))
+            cm._A = []
+            plt.colorbar(cm)
+
+
+
+def visualize(edge_index, edge_mask, y=None,
+                           threshold=None,**kwargs):
+        if edge_mask is not None:
+            assert edge_mask.size(0) == edge_index.size(1)
+        
+        if threshold is not None:
+            print('Edge Threshold:',threshold)
+            edge_mask = (edge_mask >= threshold).to(torch.float)
+
+        subset=[]
+        edge_list=[]
+        if edge_mask is not None:
+            for index,mask in enumerate(edge_mask):
+                node_a = edge_index[0,index]
+                node_b = edge_index[1,index]
+                if node_a not in subset:
+                    subset.append(node_a.cpu().item())
+                if node_b not in subset:
+                    subset.append(node_b.cpu().item())
+                if mask:
+                    edge_list.append((edge_index[0,index].cpu().item(),edge_index[1,index].cpu().item()))
+        else:
+            for index,node in edge_index:
+                node_a = edge_index[0,index]
+                node_b = edge_index[1,index]
+                if node_a not in subset:
+                    subset.append(node_a.cpu().item())
+                if node_b not in subset:
+                    subset.append(node_b.cpu().item())
+                edge_list.append((edge_index[0,index].cpu().item(),edge_index[1,index].cpu().item()))
+
+        
+        if y is None:
+            y = torch.zeros(edge_index.max().item() + 1,
+                            device=edge_index.device)
+        else:
+            y = y[subset].to(torch.float) / y.max().item()
+        
+        data = Data(edge_index=edge_index.cpu(), att=edge_mask, y=y,
+                    num_nodes=y.size(0)).to('cpu')
+
+        G = to_networkx(data, node_attrs=['y'], edge_attrs=['att'])
+#         mapping = {k: i for k, i in enumerate(subset.tolist())}
+        mapping = {k: i for k, i in enumerate(subset)}
+#         print(mapping)
+#         G = nx.relabel_nodes(G, mapping)
+
+        kwargs['with_labels'] = kwargs.get('with_labels') or True
+        kwargs['font_size'] = kwargs.get('font_size') or 10
+        kwargs['node_size'] = kwargs.get('node_size') or 200
+        kwargs['cmap'] = kwargs.get('cmap') or 'cool'
+
+#         pos = nx.kamada_kawai_layout(G)
+        pos = nx.spectral_layout(G)
+        ax = plt.figure(figsize=(20,10))
+#         for source, target, data in G.edges(data=True):
+#             ax.annotate(
+#                 '', xy=pos[target], xycoords='data', xytext=pos[source],
+#                 textcoords='data', arrowprops=dict(
+#                     arrowstyle="-",
+#                     alpha=max(data['att'], 0.1),
+#                     shrinkA=sqrt(kwargs['node_size']) / 2.0,
+#                     shrinkB=sqrt(kwargs['node_size']) / 2.0,
+# #                     connectionstyle="arc3,rad=0.1",
+#                 ))
+# #         if node_feature_mask is not None:
+        nx.draw_networkx_nodes(G, pos, **kwargs)
+#         print("edge_index:",edge_index.cpu())
+#         print("G.edges:",G.edges)
+        color = np.array(edge_mask.cpu())
+    
+#         cmap = plt.get_cmap('gist_rainbow',color)
+#         nx.draw(nx_G_undirected,pos,with_labels=False,node_size=500,font_size=14,node_color=node_type,cmap=cmap,vmin = vmin,vmax=vmax)
+        
+        nx.draw_networkx_edges(G, pos,edgelist=edge_list,
+                       width=3, alpha=0.5)
+        nx.draw_networkx_labels(G, pos, **kwargs)
+        plt.axis('off')
+        return plt
+
+def getEdgeList(DGLGraph):
+    a = DGLGraph.edges()[0].tolist()
+    b = DGLGraph.edges()[1].tolist()
+    edges = list(zip(a,b))
+    edges = np.array(edges)
+    
+    return edges
+
+def AdjToEdgelist(adj):
+    row = np.where(adj>0)[0]
+    col = np.where(adj>0)[1]
+    
+    edge_list = zip(row,col)
+    
+    return edge_list
+
+# def EdgelistToAdj(edge_list):
+#     for e0,e1 in edge_list:
+
+
+def read_graphfile(datadir, dataname, max_nodes=None, edge_labels=False):
+    """ Read data from https://ls11-www.cs.tu-dortmund.de/staff/morris/graphkerneldatasets
+        graph index starts with 1 in file
+
+    Returns:
+        List of networkx objects with graph and node labels
+    """
+    prefix = os.path.join(datadir, dataname)
+    filename_graph_indic = prefix + "_graph_indicator.txt"
+    # index of graphs that a given node belongs to
+    graph_indic = {}
+    with open(filename_graph_indic) as f:
+        i = 0
+        for line in f:
+            line = line.strip("\n")
+            graph_indic[i] = int(line)
+            i += 1
+
+    filename_nodes = prefix + "_node_labels.txt"
+    node_labels = []
+    min_label_val = None
+    try:
+        with open(filename_nodes) as f:
+            for line in f:
+                line = line.strip("\n")
+                l = int(line)
+                node_labels += [l]
+                if min_label_val is None or min_label_val > l:
+                    min_label_val = l
+        # assume that node labels are consecutive
+        num_unique_node_labels = max(node_labels) - min_label_val + 1
+        node_labels = [l - min_label_val for l in node_labels]
+    except IOError:
+        print("No node labels")
+
+    filename_node_attrs = prefix + "_node_attributes.txt"
+    node_attrs = []
+    try:
+        with open(filename_node_attrs) as f:
+            for line in f:
+                line = line.strip("\s\n")
+                attrs = [
+                    float(attr) for attr in re.split("[,\s]+", line) if not attr == ""
+                ]
+                node_attrs.append(np.array(attrs))
+    except IOError:
+        print("No node attributes")
+
+    label_has_zero = False
+    filename_graphs = prefix + "_graph_labels.txt"
+    graph_labels = []
+
+    label_vals = []
+    try:
+        with open(filename_graphs) as f:
+            for line in f:
+                line = line.strip("\n")
+                val = int(line)
+                if val not in label_vals:
+                    label_vals.append(val)
+                graph_labels.append(val)
+    except IOError:
+        print("No  graph labels")
+    label_map_to_int = {val: i for i, val in enumerate(label_vals)}
+    graph_labels = np.array([label_map_to_int[l] for l in graph_labels])
+
+    if edge_labels:
+        # For Tox21_AHR we want to know edge labels
+        filename_edges = prefix + "_edge_labels.txt"
+        edge_labels = []
+
+        edge_label_vals = []
+        with open(filename_edges) as f:
+            for line in f:
+                line = line.strip("\n")
+                val = int(line)
+                if val not in edge_label_vals:
+                    edge_label_vals.append(val)
+                edge_labels.append(val)
+
+        edge_label_map_to_int = {val: i for i, val in enumerate(edge_label_vals)}
+
+    filename_adj = prefix + "_A.txt"
+    adj_list = {i: [] for i in range(0, len(graph_labels))}
+    # edge_label_list={i:[] for i in range(1,len(graph_labels)+1)}
+    index_graph = {i: [] for i in range(0, len(graph_labels))}
+    num_edges = 0
+    with open(filename_adj) as f:
+        for line in f:
+            line = line.strip("\n").split(",")
+            e0, e1 = (int(line[0].strip(" ")), int(line[1].strip(" ")))
+            adj_list[graph_indic[e0]].append((e0, e1))
+            index_graph[graph_indic[e0]] += [e0, e1]
+            # edge_label_list[graph_indic[e0]].append(edge_labels[num_edges])
+            num_edges += 1
+    for k in index_graph.keys():
+        index_graph[k] = [u - 1 for u in set(index_graph[k])]
+
+    graphs = []
+    dgraphs=[]
+    for i in range(0, len(adj_list)):
+        # indexed from 0 here
+        G = nx.from_edgelist(adj_list[i])
+        DG=nx.DiGraph()
+        DG.add_edges_from(adj_list[i])
+        # add features and labels
+        G.graph["label"] = graph_labels[i]
+        DG.graph["label"] = graph_labels[i]
+        # Special label for aromaticity experiment
+        # aromatic_edge = 2
+        # G.graph['aromatic'] = aromatic_edge in edge_label_list[i]
+
+        for u in G.nodes():
+            if len(node_labels) > 0:
+                # node_label_one_hot = [0] * num_unique_node_labels
+                node_label = node_labels[u]
+                # node_label_one_hot[node_label] = 1
+                G.nodes[u]["label"] = node_label
+            if len(node_attrs) > 0:
+                G.nodes[u]["feat"] = node_attrs[u]
+
+        for u in DG.nodes():
+            if len(node_labels) > 0:
+                # node_label_one_hot = [0] * num_unique_node_labels
+                node_label = node_labels[u]
+                # node_label_one_hot[node_label] = 1
+                DG.nodes[u]["label"] = node_label
+            if len(node_attrs) > 0:
+                DG.nodes[u]["feat"] = node_attrs[u]
+        if len(node_attrs) > 0:
+            G.graph["feat_dim"] = node_attrs[0].shape[0]
+
+        # relabeling
+        mapping = {}
+        it = 0
+        if float(nx.__version__) < 2.0:
+            for n in G.nodes():
+                mapping[n] = it
+                it += 1
+        else:
+            for n in G.nodes:
+                mapping[n] = it
+                it += 1
+        # indexed from 0
+        graphs.append(nx.relabel_nodes(G, mapping))
+        dgraphs.append(nx.relabel_nodes(DG, mapping))
+    return graphs,dgraphs
+
+def compress_graph(G):
+    node_to_delete=[]
+    for u in G.nodes:
+        if(not u in node_to_delete):
+            neighbors = G.neighbors(u)
+            for n in neighbors:
+                if((u!=n)&(G.nodes[u]['label']==G.nodes[n]['label'])):
+#                     print(u,n)
+                    G = nx.contracted_nodes(G, u, n)
+                    node_to_delete.append(n)
+    
+    return G
---
+++ models.py
@@ -0,0 +1,99 @@
+import random
+import os
+import matplotlib.pyplot as plt
+import networkx as nx
+import csv
+import numpy as np
+
+import dgl
+import dgl.function as fn
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torch
+from torch_geometric.data import InMemoryDataset
+from torch_geometric.nn import GCNConv,GATConv
+import dgl.function as fn
+import torch
+import torch.nn as nn
+
+
+def collate(samples):
+    # The input `samples` is a list of pairs
+    #  (graph, label).
+    graphs, labels = map(list, zip(*samples))
+    batched_graph = dgl.batch(graphs)
+    return batched_graph, torch.tensor(labels)
+
+
+
+
+# Sends a message of node feature h.
+msg = fn.copy_src(src='h', out='m')
+        
+def reduce(nodes):
+    """Take an average over all neighbor node features hu and use it to
+    overwrite the original node feature."""
+    accum = torch.mean(nodes.mailbox['m'], 1)
+    return {'h': accum}
+
+class NodeApplyModule(nn.Module):
+    """Update the node feature hv with ReLU(Whv+b)."""
+    def __init__(self, in_feats, out_feats, activation):
+        super(NodeApplyModule, self).__init__()
+        self.linear = nn.Linear(in_feats, out_feats)
+        self.activation = activation
+
+    def forward(self, node):
+        h = self.linear(node.data['h'])
+        h = self.activation(h)
+        return {'h' : h}
+
+class GCN(nn.Module):
+    def __init__(self, in_feats, out_feats, activation):
+        super(GCN, self).__init__()
+        self.apply_mod = NodeApplyModule(in_feats, out_feats, activation)
+
+    def forward(self, g, feature):
+        # Initialize the node features with h.
+        g.ndata['h'] = feature
+        g.update_all(msg, reduce)
+        g.apply_nodes(func=self.apply_mod)
+        return g.ndata.pop('h')
+
+class PYGNet(torch.nn.Module):
+    def __init__(self,in_dim, hidden_dim, n_classes):
+        super(PYGNet, self).__init__()
+#         self.lin = Sequential(Linear(10, 10))
+        self.conv1 = GCNConv(in_dim, hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, hidden_dim)
+        self.classify = nn.Linear(hidden_dim, n_classes)
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.conv1(x, edge_index))
+#         x = F.dropout(x, training=self.training)
+        x = F.relu(self.conv2(x, edge_index))
+        x = torch.mean(x, 0, True)
+        return self.classify(x)
+    
+class dglmodel(nn.Module):
+    def __init__(self, in_dim, hidden_dim, n_classes):
+        super(dglmodel, self).__init__()
+
+        self.layers = nn.ModuleList([
+            GCN(in_dim, hidden_dim, F.relu),
+            GCN(hidden_dim, hidden_dim, F.relu)])
+        self.classify = nn.Linear(hidden_dim, n_classes)
+
+    def forward(self, g):
+        # For undirected graphs, in_degree is the same as
+        # out_degree.
+        h = g.in_degrees().view(-1, 1).float().cuda()
+        for conv in self.layers:
+            h = conv(g, h)
+        g.ndata['h'] = h
+        hg = dgl.mean_nodes(g, 'h')
+        return self.classify(hg)
+
